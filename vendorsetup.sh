8328c896bc2a96443a95b43ca0f7743b9f9008e3
omraj12 <rajesh.doulagar8@gmail.com>
2022-09-23 05:57:55 +0000

    haydn: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@ rm -rf kernel/xiaomi/haydn
 git clone --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_haydn kernel/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_haydn vendor/xiaomi/haydn
-
-rm -rf vendor/xiaomi/haydn-miuicamera
-git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-miuicamera -b twelve vendor/xiaomi/haydn-miuicamera
+git clone --depth=1 https://gitlab.com/omraj12/projectelixir_vendor_xiaomi_haydn.git vendor/xiaomi/haydn
 
 rm -rf vendor/xiaomi/haydn-firmware
 git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-firmware vendor/xiaomi/haydn-firmware
+
+rm -rf vendor/qcom/opensource/healthd-ext
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_healthd-ext.git vendor/qcom/opensource/healthd-ext
